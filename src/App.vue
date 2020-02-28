f0966eb20a4b88c32188d95fe6bd864a8a64672a
Judson de Almeida Rodrigues <judson.rodrigues@geap.com.br>
2020-02-28 14:24:22 -0300

    router link

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
 		</div>
 
 		<div class="container">
+			<router-link to="/" class="btn btn-info mb-2 mr-2">Home</router-link>
+			<router-link :to="{ path:'/contatos'}" class="btn btn-info mb-2 mr-2">Contatos</router-link>
+
 			<!-- <h3 class="font-weight-light">Contatos</h3> -->
 			<router-view></router-view>
 		</div>

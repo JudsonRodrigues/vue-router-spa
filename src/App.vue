16908977ef89433168d018c0d4dc67735729444e
Judson de Almeida Rodrigues <judson.rodrigues@geap.com.br>
2020-02-28 16:51:50 -0300

    capturando parametros da rota

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@
 			<router-link to="/" class="btn btn-info mb-4 mr-2">Home</router-link>
 			<router-link :to="{ path:'/contatos'}" class="btn btn-info mb-4 mr-2">Contatos</router-link>
 
+			<router-link to="/Contatos/1" class="btn btn-info mb-4 mr-2">Contato 1</router-link>
+
+			<router-link to="/Contatos/2" class="btn btn-info mb-4 mr-2">Contato 2</router-link>
+
 			<!-- <h3 class="font-weight-light">Contatos</h3> -->
 			<router-view></router-view>
 		</div>
--- src/views/contatos/ContatoDetalhes.vue
+++ src/views/contatos/ContatoDetalhes.vue
@@ -1,9 +1,25 @@
 <template>
-	<h3 class="font-weight-light">Detalhes sobre o contato</h3>
+	<h3 class="font-weight-light">Detalhes sobre o contato com id: {{id}}</h3>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			id: this.$route.params.id
+		};
+	},
+	// watch: {
+	// 	// eslint-disable-next-line no-unused-vars
+	// 	$route(to, from) {
+	// 		this.id = to.params.id;
+	// 	}
+	// },
+	// eslint-disable-next-line no-unused-vars
+	beforeRouteUpdate(to, from, next) {
+		this.id = to.params.id;
+		next();
+	},
 	created() {
 		console.log("Parâmetros da rota", this.$route.params);
 	}
